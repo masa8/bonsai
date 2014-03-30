1a193b27500a9a6646b1701a1d763c9b14949e7b
masa <masa.shimomatsu@gmail.com>
2014-03-31 00:14:22 +0900

    Update to make it simple

--- bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
+++ bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
@@ -1,21 +1,8 @@
 package aruite.tanoshiku.bousaikunren;
 
-
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import android.app.Activity;
-import android.app.Dialog;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 
 
@@ -28,12 +15,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Getting reference to the SupportMapFragment of activity_main.xml
+        
         MapFragment fm = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-
-        // Getting GoogleMap object from the fragment
-        googleMap = fm.getMap();
-        googleMap.setMyLocationEnabled(true);
-   }
+        if ( fm != null ){
+            googleMap = fm.getMap();
+            googleMap.setMyLocationEnabled(true);        	
+        }
+    }
 }
